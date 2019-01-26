5a8e2c3ed1f5efd30209e703af401b0aaaa4279d
deliacodes <deliacodes@gmail.com>
2019-01-25 23:19:31 -0800

    Adding First component and test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import DisplayGame from "./Display";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it("Renders DisplayGame without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<DisplayGame />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/Display.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DisplayGame() {
+  return <div className="display" />;
+}
