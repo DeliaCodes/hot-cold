fb0eeb3aade687e4acd4a67efaa45aa00046089e
deliacodes <deliacodes@gmail.com>
2019-01-26 16:05:57 -0800

    Hooking up the Game Display to the main screen.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import DisplayGame from "./DisplayGame";
 
 class App extends Component {
   render() {
-    return <main className="main" />;
+    return (
+      <main className="main">
+        <nav className="nav">
+          <a href="">What?</a>
+          <a href="">+New Game</a>
+        </nav>
+        <h1>HOT or COLD</h1>
+        <h3>a number guessing game</h3>
+        <DisplayGame />
+      </main>
+    );
   }
 }
 
